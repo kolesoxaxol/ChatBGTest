5b9006cd074858fa03a1be2368f3d909d98ec18f
kolesoxaxol <kolesoxaxol@mail.ru>
2016-04-23 11:53:28 +0300

    test firt commit

---
+++ ChatBG/ChatBG/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatBG.MvcApplication" Language="C#" %>
---
+++ ChatBG/ChatBG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatBG.Startup))]
+namespace ChatBG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+              ConfigureAuth(app); 
+        }
+    }
+}
